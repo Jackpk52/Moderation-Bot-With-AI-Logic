dd1afc7209c52f74cd257543a9b2c5d8ffe11366
Mk-Brothers-Official <162438012+Jackpk52@users.noreply.github.com>
2025-09-21 15:59:48 +0500

    Update prompt.js

--- prompt.js
+++ prompt.js
@@ -1,6 +1,7 @@
 // Edit this file with your bot details
 module.exports = {
-  token: "MTQxNjQxMTY3OTk3NzExNTg0OA.GkUF9l.7qL56ZFKL2Gv7VoPZNV0GAN2i_3bPnpFbbNP-Q", // 🔑 Your Discord bot token
-  clientId: "1416411679977115848", // 🔑 Your application (client) ID
-  hfApiKey: "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" // 🔑 Hugging Face key
+  token: "Fill_In_Your_bot_token_here", // 🔑 Your Discord bot token
+  clientId: "Your_client_ID_here", // 🔑 Your application (client) ID
+  hfApiKey: "your_huggingface_api_key" // 🔑 Hugging Face key
 };
+
